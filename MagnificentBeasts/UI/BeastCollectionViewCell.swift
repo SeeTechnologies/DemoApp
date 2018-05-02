e765a9aa972c652209b26264248ae7833b8b75ed
Lee Sitek <seetechnologiesinc@gmail.com>
2018-05-01 22:23:55 -0600

    Basic setup of MyBeasts collection view

---
+++ MagnificentBeasts/UI/BeastCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BeastCollectionViewCell.swift
+//  MagnificentBeasts
+//
+//  Created by LS on 2018-05-01.
+//  Copyright © 2018 SeeTechnologies Inc. All rights reserved.
+//
+
+import UIKit
+
+class BeastCollectionViewCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var nameLabel: UILabel!
+}
