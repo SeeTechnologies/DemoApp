97b88d373e3d2f5eaa82348b2b56de5933b394ce
Lee Sitek <seetechnologiesinc@gmail.com>
2018-05-03 23:58:55 -0600

    Minor UI tweaks

--- MagnificentBeasts/ViewController.swift
+++ MagnificentBeasts/ViewController.swift
@@ -14,7 +14,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.isHidden = false
     }
 
     override func didReceiveMemoryWarning() {
